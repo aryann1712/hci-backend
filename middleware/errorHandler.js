3e5bd54d64dc41523e737d089fd91e0595c86cc4
rounak <rounak>
2025-03-21 12:03:17 +0530

    fixed error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,54 +1,55 @@
 const {constants} = require("../utils/constants");
 
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 400;
-    switch (statusCode) {
+    res.status(500).json({ error: (err.message|| err) });
+    // const statusCode = res.statusCode ? res.statusCode : 400;
+    // switch (statusCode) {
 
-        case constants.VALIDATION_ERROR:
-            res.json({
-                status: constants.VALIDATION_ERROR,
-                errorMessage: err.message, 
-                // stacktrace: err.stack
-            });
-            break;
+    //     case constants.VALIDATION_ERROR:
+    //         res.json({
+    //             status: constants.VALIDATION_ERROR,
+    //             errorMessage: err.message, 
+    //             // stacktrace: err.stack
+    //         });
+    //         break;
     
-        case constants.NOT_FOUND:
-            res.json({
-                status: constants.NOT_FOUND,
-                errorMessage: err.message, 
-                // stacktrace: err.stack
-            });
-            break;
+    //     case constants.NOT_FOUND:
+    //         res.json({
+    //             status: constants.NOT_FOUND,
+    //             errorMessage: err.message, 
+    //             // stacktrace: err.stack
+    //         });
+    //         break;
     
-        case constants.UNAUTHORIZED:
-            res.json({
-                status: constants.UNAUTHORIZED,
-                errorMessage: err.message, 
-                // stacktrace: err.stack
-            });
-            break;
+    //     case constants.UNAUTHORIZED:
+    //         res.json({
+    //             status: constants.UNAUTHORIZED,
+    //             errorMessage: err.message, 
+    //             // stacktrace: err.stack
+    //         });
+    //         break;
     
-        case constants.FORBIDDEN:
-            res.json({
-                status: constants.FORBIDDEN,
-                errorMessage: err.message, 
-                // stacktrace: err.stack
-            });
-            break;
+    //     case constants.FORBIDDEN:
+    //         res.json({
+    //             status: constants.FORBIDDEN,
+    //             errorMessage: err.message, 
+    //             // stacktrace: err.stack
+    //         });
+    //         break;
 
     
-        // case constants.SERVER_ERROR:
-        //     res.json({
-        //         status: constants.SERVER_ERROR,
-        //         errorMessage: err.message, 
-        //         // stacktrace: err.stack
-        //     });
-        //     break;
+    //     // case constants.SERVER_ERROR:
+    //     //     res.json({
+    //     //         status: constants.SERVER_ERROR,
+    //     //         errorMessage: err.message, 
+    //     //         // stacktrace: err.stack
+    //     //     });
+    //     //     break;
     
-        default:
-            console.log("No error, All good!",err);
-            break;
-    }
+    //     default:
+    //         console.log("No error, All good!",err);
+    //         break;
+    // }
 }
 
 
